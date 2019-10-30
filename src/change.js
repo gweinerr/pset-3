b466cbb41556f702e64b7949acbb0c73305a77f5
Gabrielle Weiner <gweiner@ucvts.org>
2019-10-30 12:36:42 -0400

    finished exercise one.

--- src/change.js
+++ src/change.js
@@ -6,9 +6,26 @@ const MAX = 0.99;
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("\nInvalid.");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("\nInvalid.");
 } else {
-    // write your code for making change here
+    const QUARTER = 0.25;
+    const DIME = 0.10;
+    const NICKEL = 0.05;
+    const PENNY = 0.01;
+
+    let QuarterAmount =  Math.floor(amount/QUARTER);
+    let QuarterRemainder = amount % QUARTER;
+
+    let DimeAmount = Math.floor(QuarterRemainder/DIME);
+    let DimeRemainder = QuarterRemainder % DIME;
+
+    let NickelAmount = Math.floor(DimeRemainder/NICKEL);
+    let NickelRemainder = DimeRemainder % NICKEL;
+
+    let PennyAmount = Math.ceil(NickelRemainder/PENNY);
+
+    console.log("\n" + QuarterAmount + " quarters, " + DimeAmount + " dimes, " + NickelAmount +  " nickels, and " + PennyAmount + " pennies.\n");
+
 }
