e4d914f919dd374ba78958548af8ea7c0bf84822
EMILY STEINER <emilysteiner@EMILYs-iMac.local>
2019-11-03 15:42:21 -0500

    finished exercise 10 and added to other exercises

---
+++ src/months.js
@@ -0,0 +1,4 @@
+const readlineSync = require("readline-sync");
+ let input =
+ if (input == "feb" || input == "february")
+ //make it toLowerCase before
---
+++ src/temperature.js
@@ -0,0 +1,40 @@
+const readlineSync = require("readline-sync");
+
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+const temperature = readlineSync.question("\nEnter a temperature: ");
+let scale = readlineSync.question("Enter a scale: ");
+scale = scale.toLowerCase();
+
+if (temperature > MAX || temperature < MIN) {
+  console.log("\nInvalid.\n");
+} else if (scale != "f" && scale != "c" && scale && "k") {
+  console.log("\nInvalid.\n");
+} else if (!Number.isInteger(temperature)) {
+  console.log("\nInvalid.\n")
+} else if (scale == "f") {
+  if (temperature < 32) {
+    console.log("\nSolid.\n");
+  } else if (temperature >= 32 && temperature <= 212) {
+    console.log("\nLiquid.\n");
+  } else {
+    console.log("\nGas.\n");
+  }
+} else if (scale == "c") {
+  if (temperature < 32) {
+    console.log("\nSolid.\n");
+  } else if (temperature >= 32 && temperature <= 212) {
+    console.log("\nLiquid.\n");
+  } else {
+    console.log("\nGas.\n");
+  }
+} else if (scale == "k") {
+  if (temperature < 32) {
+    console.log("\nSolid.\n");
+  } else if (temperature >= 32 && temperature <= 212) {
+    console.log("\nLiquid.\n");
+  } else {
+    console.log("\nGas.\n");
+  }
+}
